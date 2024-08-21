688d9ecd49a860ed03b63b8160499f4402f7ebfd
Yash Gupta <76595447+conqryash007@users.noreply.github.com>
2024-08-22 00:05:55 +0530

    update

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -5,7 +5,19 @@ const multer = require("multer");
 
 const uploadToS3 = require("./../middleware/uploadTos3");
 
-const upload = multer({ dest: "uploads/" });
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+
+    console.log("cb desti", file);
+  },
+  filename: function (req, file, cb) {
+    console.log(file, "file");
+
+    cb(null, file.originalname);
+  },
+});
+let upload = multer({ storage: storage });
 
 router.post("/signup", upload.single("image"), authController.signUp);
 
