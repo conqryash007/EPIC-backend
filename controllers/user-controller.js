ae3068daa8cd17b411aa180b52ff63763c9a5d05
TusharAhinave <tusharahinave71@gmail.com>
2024-08-23 23:46:41 +0530

    user and quiz updated

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,48 +25,91 @@ module.exports.getUsers = async (req, res) => {
 // Get a user by ID
 module.exports.getUserById = async (req, res) => {
   try {
-    const { mobile, email, verify, uid } = req.body;
+    const { params } = req.body; // Extract 'params' from the body
+    const { mobile, email, verify } = params; // Extract 'mobile', 'email', and 'verify' from 'params'
+
+    // Helper function to handle user responses
+    const handleUserResponse = (user, msg) => {
+      if (user.length > 0) {
+        const response = { ok: true, data: user[0], msg };
+        if (!verify) {
+          response.token = generateToken(user[0]._id);
+        }
+        return res.status(200).json(response);
+      }
+    };
 
     if (mobile) {
       const mobUser = await User.find({ mobile });
 
-      if (mobUser.length > 0) {
-        if (verify) {
-          const token = generateToken(mobUser[0]._id);
-          return res
-            .status(200)
-            .json({ ok: true, data: mobUser[0], msg: "User exists", token });
-        } else {
-          return res
-            .status(200)
-            .json({ ok: true, data: mobUser[0], msg: "User exists" });
-        }
+      if (mobUser?.length) {
+        handleUserResponse(mailUser, "Login Successfully..!");
+      } else {
+        return res
+          .status(200)
+          .json({ ok: false, data: {}, msg: "User does not exist" });
       }
     }
 
     if (email) {
       const mailUser = await User.find({ email });
 
-      if (mailUser.length > 0) {
-        if (verify) {
-          const token = generateToken(mailUser[0]._id);
-          return res
-            .status(200)
-            .json({ ok: true, data: mailUser[0], msg: "User exists", token });
-        } else {
-          return res
-            .status(200)
-            .json({ ok: true, data: mailUser[0], msg: "User exists" });
-        }
+      if (mailUser.length) {
+        handleUserResponse(mailUser, "Login Successfully..!");
+      } else {
+        return res
+          .status(200)
+          .json({ ok: false, data: {}, msg: "User does not exist" });
       }
     }
-
-    return res
-      .status(200)
-      .json({ ok: true, data: {}, msg: "User does not exists" });
   } catch (error) {
     res.status(500).json({ ok: false, msg: error.message });
   }
+  // try {
+  //   const { mobile, email, verify, uid } = req.body;
+  //   console.log("body", req.body);
+
+  //   if (mobile) {
+  //     const mobUser = await User.find({ mobile });
+  //     console.log("body", mobUser, mobUser[0]._id);
+
+  //     if (mobUser.length > 0) {
+  //       if (verify) {
+  //         const token = generateToken(mobUser[0]._id);
+  //         return res
+  //           .status(200)
+  //           .json({ ok: true, data: mobUser[0], msg: "User exists", token });
+  //       } else {
+  //         return res
+  //           .status(200)
+  //           .json({ ok: true, data: mobUser[0], msg: "User exists" });
+  //       }
+  //     }
+  //   }
+
+  //   if (email) {
+  //     const mailUser = await User.find({ email });
+
+  //     if (mailUser.length > 0) {
+  //       if (verify) {
+  //         const token = generateToken(mailUser[0]._id);
+  //         return res
+  //           .status(200)
+  //           .json({ ok: true, data: mailUser[0], msg: "User exists", token });
+  //       } else {
+  //         return res
+  //           .status(200)
+  //           .json({ ok: true, data: mailUser[0], msg: "User exists" });
+  //       }
+  //     }
+  //   }
+
+  //   return res
+  //     .status(200)
+  //     .json({ ok: true, data: {}, msg: "User does not exists" });
+  // } catch (error) {
+  //   res.status(500).json({ ok: false, msg: error.message });
+  // }
 };
 
 // Update a user
