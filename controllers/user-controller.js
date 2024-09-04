a25217e1e15731100ec7d9620efdef0302d82507
Yash Gupta <76595447+conqryash007@users.noreply.github.com>
2024-09-04 12:06:40 +0530

    added update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,7 @@
 const User = require("./../models/User");
 const jwt = require("jsonwebtoken");
+const AWS = require("aws-sdk");
+const fs = require("fs");
 
 require("dotenv").config();
 
@@ -121,17 +123,86 @@ module.exports.getUsersByUserId = async (req, res) => {
   }
 };
 
+const doesUserExists = async (mobile, email) => {
+  if (mobile) {
+    const mobUser = await User.find({ mobile });
+
+    if (mobUser.length > 0) {
+      return true;
+    }
+  }
+
+  if (email) {
+    const mailUser = await User.find({ email });
+
+    if (mailUser.length > 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
 // Update a user
 module.exports.updateUser = async (req, res) => {
   try {
     const userId = req.user.id;
-    const user = await User.findByIdAndUpdate(userId, req.body, {
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(200).json({ message: "User not found" });
+
+    const { mobile, email } = req.body;
+    const isPresent = await doesUserExists(mobile, email);
+    if (isPresent) {
+      return res.send({ ok: false, msg: "User Already Exits" });
     }
-    res.status(200).json({ ok: true, data: user });
+
+    const file = req.file;
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) throw err;
+        const params = {
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          Key: file.originalname,
+          Body: data,
+          ContentType: file.mimetype,
+          ACL: "public-read",
+        };
+        s3.upload(params, async (err, data) => {
+          console.log("upload data", data, params);
+
+          if (err) {
+            console.log("Error uploading file", err);
+            throw new Error("Error uploading file");
+          } else {
+            const userRes = await User.findByIdAndUpdate(userId, {
+              ...req.body,
+              profile_pic: data.Location,
+            });
+
+            res.send({
+              ok: true,
+              msg: "User Updated Successfully!",
+              userRes,
+            });
+            fs.unlinkSync(file.path);
+            return;
+          }
+        });
+      });
+    } else {
+      const userRes = await User.findByIdAndUpdate(userId, {
+        ...req.body,
+      });
+
+      return res.send({
+        ok: true,
+        msg: "User Updated Successfully!",
+        userRes,
+      });
+    }
   } catch (error) {
     res.status(400).json({ ok: false, msg: error.message });
   }
--- routes/user-route.js
+++ routes/user-route.js
@@ -5,6 +5,19 @@ const authenticateToken = require("./../middleware/authenticateToken");
 
 const jwtAuth = require("./../middleware/jwtAuth");
 const uploadToS3 = require("./../middleware/uploadTos3");
+const multer = require("multer");
+
+const uploadDir = "./uploads";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 // USER
 router.get("/", jwtAuth, userController.getUsers);
@@ -12,7 +25,12 @@ router.get("/:id", jwtAuth, userController.getUsersByUserId);
 
 router.post("/", userController.getUserById);
 
-router.post("/update", jwtAuth, userController.updateUser);
+router.post(
+  "/update",
+  upload.single("image"),
+  jwtAuth,
+  userController.updateUser
+);
 
 router.post("/delete", jwtAuth, userController.deleteUser);
 
